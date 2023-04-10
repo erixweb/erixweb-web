5461d1dd481a43b3b9cf04df017adc996400ad66
= <erikmorenodev@gmail.com>
2023-04-10 15:10:11 +0200

    Fix projects link, add transition to images,

--- store/projects.js
+++ store/projects.js
@@ -3,42 +3,42 @@ export const projects = [
         id: "1",
         name: "AstroJS Wiki",
         desc: "Astro Wiki es un proyecto para encontrar la solución a dudas de Astro.",
-        url: "https://erixweb.github.io",
+        url: "https://astrowiki.vercel.app",
         image: "/images/projects/astrowiki_thumb.webp"
     },
     {
         id: "2",
         name: "WindfulCSS",
         desc: "Windful es una librería de CSS basada en tailwind muy ligera.",
-        url: "https://erixweb.github.io",
+        url: "https://windful.vercel.app",
         image: "/images/projects/windfullthumb.webp"
     },
     {
         id: "3",
         name: "Pywarp",
         desc: "Pywarp es una terminal con comandos básicos para desarolladores web.",
-        url: "https://erixweb.github.io",
+        url: "https://erixweb.github.io/pycmd/",
         image: "/images/projects/pywarp_thumb.webp"
     },
     {
         id: "4",
         name: "WetSpain",
         desc: "WetSpain es una página para ver el clima en diferentes ciudades de España.",
-        url: "https://erixweb.github.io",
+        url: "https://wetspain.vercel.app",
         image: "/images/projects/wetspain_thumb.webp"
     },
     {
         id: "5",
         name: "Furndev",
         desc: "Furndev es una página estática con muebles.",
-        url: "https://erixweb.github.io",
+        url: "https://furndev.pages.dev",
         image: "/images/projects/furndev.webp"
     },
     {
         id: "6",
         name: "LSXDom",
         desc: "LSX es un clone de JSX hecho con JavaScript Vanilla y menos de 2.5kB.",
-        url: "https://erixweb.github.io",
+        url: "https://lsxdom.vercel.app/",
         image: "/images/projects/lsxthumb.webp"
     },
 ]
