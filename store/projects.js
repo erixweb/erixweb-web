82677916271743a550e399738c802c862bae00e3
= <erikmorenodev@gmail.com>
2023-06-19 11:52:21 +0200

    Add github btn

--- store/projects.js
+++ store/projects.js
@@ -4,6 +4,7 @@ export const projects = [
         name: "AstroJS Wiki",
         desc: "Astro Wiki es un proyecto para encontrar la solución a dudas de Astro.",
         url: "https://astrowiki.vercel.app",
+        github: "https://github.com/erixweb/astrowiki",
         image: "/images/projects/astrowiki_thumb.webp"
     },
     {
@@ -11,6 +12,7 @@ export const projects = [
         name: "WindfulCSS",
         desc: "Windful es una librería de CSS basada en tailwind muy ligera.",
         url: "https://windful.vercel.app",
+        github: "https://github.com/erixweb/windfulcss",
         image: "/images/projects/windfullthumb.webp"
     },
     {
@@ -18,6 +20,7 @@ export const projects = [
         name: "Zastate",
         desc: "Zastate es un manejador de estados que pesa tan solo 1.2kB.",
         url: "https://zastate.deno.dev",
+        github: "https://github.com/erixweb/zastate",
         image: "/images/projects/zastate_thumb.avif"
     },
     {
@@ -25,6 +28,7 @@ export const projects = [
         name: "Pywarp",
         desc: "Pywarp es una terminal con comandos básicos para desarolladores web.",
         url: "https://erixweb.github.io/pycmd/",
+        github: "https://github.com/erixweb/pycmd",
         image: "/images/projects/pywarp_thumb.webp"
     },
     {
@@ -32,6 +36,7 @@ export const projects = [
         name: "WetSpain",
         desc: "WetSpain es una página para ver el clima en diferentes ciudades de España.",
         url: "https://wetspain.vercel.app",
+        github: "https://github.com/erixweb/wetspain",
         image: "/images/projects/wetspain_thumb.webp"
     },
     {
@@ -39,6 +44,7 @@ export const projects = [
         name: "Furndev",
         desc: "Furndev es una página estática con muebles.",
         url: "https://furndev.pages.dev",
+        github: "https://github.com/erixweb/furndev",
         image: "/images/projects/furndev.webp"
     },
     {
@@ -46,6 +52,7 @@ export const projects = [
         name: "LSXDom",
         desc: "LSX es un clone de JSX hecho con JavaScript Vanilla y menos de 2.5kB.",
         url: "https://lsxdom.vercel.app/",
+        github: "https://github.com/erixweb/lsx",
         image: "/images/projects/lsxthumb.webp"
     },
 ]
