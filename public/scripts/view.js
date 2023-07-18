a079fdd0a7ada2ae0c41b70f0aad8c3382ca256f
erixweb <erikmorenodev@gmail.com>
2023-07-18 10:03:24 +0200

    Improve portfolio

---
+++ public/scripts/view.js
@@ -0,0 +1,40 @@
+const getPageBody = async (url) => {
+    const req = await fetch(url)
+    const res = await req.text()
+
+    const body = res.match(/<body>([\s\S]*)<\/body>/i)[1]
+
+    return body
+}
+const viewTransitionSupported = () => {
+  return Boolean(document.startViewTransition)
+}
+const updateDOM = (html) => {
+    document.body.innerHTML = html
+}
+
+window.navigation.addEventListener("navigate", (event) => {
+
+    if (!viewTransitionSupported()) return
+  
+    const url = new URL(event.destination.url)
+  
+    if (location.origin !== url.origin) return
+  
+    event.intercept({
+      async handler() {
+        const data = await getPageBody(url.pathname)
+  
+        document.startViewTransition(async () => {
+          await updateDOM(data)
+          document.documentElement.scrollTop = 0
+          if (url.pathname.startsWith("/articulo/")) {
+            const fetchScript = await fetch("/js/highlight.js")
+            const script = await fetchScript.text()
+  
+            eval(script)
+          }
+        })
+      },
+    })
+  })
