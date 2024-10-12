1b5fadf29d93ff91d855de3fd63b07aeed0caace
erixweb <erikmorenodev@gmail.com>
2024-10-12 13:40:23 +0200

    Fix error

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import cloudflare from "@astrojs/cloudflare";
 import tailwind from "@astrojs/tailwind";
 
-import preact from "@astrojs/preact";
 
 // https://astro.build/config
 export default defineConfig({
